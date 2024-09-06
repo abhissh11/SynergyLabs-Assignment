093e32aae516a1b5db16d141feee9e3fa173e5a5
dev-abhishekkr <dev.abhishekkr@gmail.com>
2024-09-06 12:14:22 +0530

    minor edit

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
     <div className="flex flex-col gap-10 items-center justify-center h-screen">
-      <h1 className="text-xl">Are You Lost Baby girl!?</h1>
+      <h1 className="text-xl">Page Not Found!</h1>
       <Link
         to="/"
         className="text-xl font-semibold bg-black text-white p-3 rounded-md"
       >
-        Return to Home
+        Return to HomePage
       </Link>
     </div>
   );
